a9ea6e38229d6ef88d7431a2c0a1da952cc0fa96
catcxx <frankchen443@gmail.com>
2016-05-23 09:52:49 +0800

    update

--- src/thread/TestThread0.java
+++ src/thread/TestThread0.java
@@ -1,33 +1,34 @@
 package thread;
 
-public class TestThread0 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ThreadTest tt = new ThreadTest();
-		Thread rt =new Thread(new RunableTest());
-		tt.start();
-		rt.start();
+	public class TestThread0 {
+	
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+			ThreadTest tt = new ThreadTest();
+			Thread rt = new Thread(new RunableTest());
+			tt.start();
+			tt.join();
+			rt.start();
+		}
+	
 	}
 
-}
-
-class ThreadTest extends Thread{
-	public void run(){
-		for(int i=0;i<100;i++){
-		System.out.println("tt:"+i);
+	class ThreadTest extends Thread {
+		public void run() {
+			for (int i = 0; i < 100; i++) {
+				System.out.println("tt:" + i);
+			}
 		}
 	}
-}
 
-class RunableTest implements Runnable{
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		for(int i=0;i<100;i++){
-			System.out.println("rt:"+i);
-			}
-	}
+	class RunableTest implements Runnable {
 	
-}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for (int i = 0; i < 100; i++) {
+				System.out.println("rt:" + i);
+			}
+		}
+	
+	}
